035b0afefe4e8fe9074e7eba9d97c9997c57de8b
ignacio.angrigiani@gmail.com <ignacio.angrigiani@gmail.com@fdcf8f4b-0583-6058-e83b-01a880107679>
2012-10-08 02:45:31 +0000

    Agregado servidor de mensajes entre el proceso principal, las entradas y la consola

--- trunk/TP1.cpp
+++ trunk/TP1.cpp
@@ -1,12 +1,16 @@
 #include "Estacionamiento.h"
+#include "Pipe.h"
+
+# define BUFFSIZE 100
 
 using namespace std;
 
 int main()
 {
+/*
 	Estacionamiento estacionamiento;
-
-	estacionamiento.run(3,2,30);
+	estacionamiento.run(10,2,30);
+*/
 
 	return 0;
 
